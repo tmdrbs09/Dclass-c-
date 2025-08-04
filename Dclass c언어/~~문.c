a8390ebd21b02d849a69d5a02f6d0aceb6eb5d13
tmdrbs_09 <aclim0410@gmail.com>
2025-08-04 16:47:59 +0900

    DAY7

---
+++ Dclass c언어/~~문.c
@@ -0,0 +1,20 @@
+#include <stido.h>
+main(void)
+{ 
+	/*
+	if(조건문) {
+	조건문을 만족할 경우 실행
+	}
+	else if(조건문2) {
+	조건문을 만족하지 않고, 다른 조건문을 만족할 경우 실행
+	}
+	else {조건문3) {
+	위 모든 조건을 만족하지 않을 경우 실행
+	*/
+
+	int number;
+	printf("숫자 입력 :	);
+	scanf_s
+	return 0;
+}
+
---
+++ Dclass c언어/비트단위 연산자.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+main(void)
+{
+	// & (and) : 대응하ㅡㄴ 각 비트의 값이 모두 1이라면 1을 반환 아니면 0
+	// 	ㅣ (or) : 대응하는 비트의 값 중 1이 하나으면 1을 반환
+	// ^ (xor0) : 대응하는 비트의 값이 다르면 1을 반환
+	// ~ (not_) : 비트의 값이 1 이면 0으로, 0 이라면 1로 반전시켜서 나타냄
+
+	int bit_five = 5; // 00000101
+	int bit_three = 3; // 00000011
+	printf("%d & %d = %d", bit_five, bit_three, bit_five & bit_three);
+
+	int bit_seven = 7; //00000111
+	printf("%d & %d", bit_seven, bit_three, bit_seven & bit_three);
+
+	int if_number = 10;
+	if (if_number == 3) {
+	
+	}
+
+		//입력받은 하루 걸음수가 10000보 일때 경우매\\
+		// 하루에 %d 걷다니 대단해여! 좋은 습과 입니다!
+	
+	int walk_count;
+	printf("당신은 하루에 몇 보 걷나요?")
+	char alpha; = 'A'
+		if (alpha) == 'B' { 
+			
+
+			int like_number >= 10; { 
+				printf("당신이 좋아하는 숫자는 10 이상이군ㅇ~~");
+
+	return 0;
+}
